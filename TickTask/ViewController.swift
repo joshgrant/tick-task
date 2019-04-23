1fe25ddcaab8c097e9f5ea7996c778f6957b9d94
Joshua Grant <formaze@pm.me>
2019-04-23 11:55:20 +0200

    Initial Commit

---
+++ TickTask/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TickTask
+//
+//  Created by Joshua Grant on 4/23/19.
+//  Copyright © 2019 joshgrant. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
